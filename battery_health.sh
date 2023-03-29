4730ae7fa593cd4e3244ff0e0b24de5bd4064876
Hazemorn <nikitmiheenko@gmail.com>
2023-03-29 22:07:37 +0300

    fix

--- battery_health.sh
+++ battery_health.sh
@@ -1,15 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 #ls /sys/class/power_supply
-echo -n "Hello! Write battery percent(%): (or type 1 for 60 % of charge or 2 for 80 % of charge)"
+echo -n "Hello! Write battery percent(%): (or type 1 for 60 % of charge or 2 for 80 % of charge) "
 read bat_percent
 
-if [$bat_percent -eq 1]; then
+if [[ $bat_percent -eq 1]]; then
 	echo 60 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
-   elif [$bat_percent -eq 2]; then
+elif [[ $bat_percent -eq 2]] then
    	echo 80 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
-   elif [$bat_percent -ge 0] && [$bat_percent -le 100]; then
+elif [[ $bat_percent -ge 0]] && [[ $bat_percent -le 100]]; then
    	echo $bat_percent | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
-   else
+else
    	echo "Something wrong. Please enter a valid max limit between 1 and 100"
 fi
