1b5b4e650c62070262db268b32ec54c6c7830cc3
Hazemorn <nikitmiheenko@gmail.com>
2023-04-10 20:31:57 +0300

    fix -ge /-eq statement

--- battery_health.sh
+++ battery_health.sh
@@ -5,17 +5,18 @@ echo -e "Before using script v.1 run follow command:\n     ls /sys/class/power_s
 echo "Contunie? (y/n)"
 read answer
 case $answer in
-	"y") echo -n "Hello! Write battery percent(%): (or type 1 for 60 % of charge or 2 for 80 % of charge) "
+	"y") 
+echo -n "Hello! Write battery percent(%): (or type 1 for 60 % of charge or 2 for 80 % of charge) "
 		read bat_percent
-		if [ $bat_percent -ge 1 ]; then
-			echo 60 | sudo tee /sys/class/power_supply/BAT?/charge_control_end_threshold
+		if [ $bat_percent -eq 1 ]; then
+			echo 60 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
 		elif [ $bat_percent == 2 ]; then
-   			echo 80 | sudo tee /sys/class/power_supply/BAT?/charge_control_end_threshold
+   			echo 80 | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
 		elif [ $bat_percent -ge 0 ] && [ $bat_percent -le 100 ]; then
-   			echo $bat_percent | sudo tee /sys/class/power_supply/BAT?/charge_control_end_threshold
+   			echo $bat_percent | sudo tee /sys/class/power_supply/BAT1/charge_control_end_threshold
 		else
    			echo "Something wrong. Please enter a valid max limit between 1 and 100"
-		fi;;
+		fi;; 
 	"n") echo "Have a nice day!";;
-	*) echo "Command is not correct. Retry again"
+	*) echo "Command is not correct. Try again"
 esac
